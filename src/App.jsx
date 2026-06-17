8b48a1560168b7f28afa240b4406c46a02066f90
Gopichand715306 <ogopi713@gmail.com>
2026-06-18 00:13:56 +0530

    Fix base path

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Footer from './components/Footer';
 function App() {
   return (
     <HospitalProvider>
-      <Router>
+      <Router basename="/hospitalmanagement">
         <div className="app-container">
           <Navbar />
           <main className="main-content">
